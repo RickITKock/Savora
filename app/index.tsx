d5f40efc99c2f65194d19703933a3facbfea0407
Rick Kock <rit.kock@gmail.com>
2025-10-04 15:34:41 +0200

    Add login button to homescreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,24 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.view}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/Login">Go to Login</Link>
+      <Link href="/login" style={styles.navButton}>
+        Go to Login
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view: { flex: 1, justifyContent: "center", alignItems: "center" },
+  navButton: {
+    marginTop: 20,
+    fontSize: 18,
+    color: "blue",
+    backgroundColor: "lightblue",
+    padding: 10,
+  },
+});
